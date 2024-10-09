b633b5673d1726215a5855437d156539f6b7a558
mstirinakter47e <mstirinakter47@gmail.com>
2024-10-09 23:03:01 +0600

    demo

---
+++ build/flutter_mdns_plugin/generated/source/buildConfig/release/eu/sndr/fluttermdnsplugin/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package eu.sndr.fluttermdnsplugin;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "eu.sndr.fluttermdnsplugin";
+  public static final String BUILD_TYPE = "release";
+}
